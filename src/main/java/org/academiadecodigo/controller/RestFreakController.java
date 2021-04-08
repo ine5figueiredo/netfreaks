36217038dc3169da3213e478215be0457c70e833
Inês Figueiredo <inesgomesfigueiredo@gmail.com>
2021-04-08 16:34:54 +0100

    Working on api

--- src/main/java/org/academiadecodigo/controller/RestFreakController.java
+++ src/main/java/org/academiadecodigo/controller/RestFreakController.java
@@ -1,9 +1,42 @@
-package controller;
+package org.academiadecodigo.controller;
 
+import org.academiadecodigo.command.FreakDTO;
+import org.academiadecodigo.converters.FreakDTOToFreak;
+import org.academiadecodigo.converters.FreakToFreakDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 import service.FreakService;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("/home")
 public class RestFreakController {
 
     private FreakService freakService;
+    private FreakDTOToFreak freakDTOToFreak;
+    private FreakToFreakDTO freakToFreakDTO;
 
+    @Autowired
+    public void setFreakService(FreakService freakService) {
+        this.freakService = freakService;
+    }
+
+    @Autowired
+    public void setFreakDTOToFreak(FreakDTOToFreak freakDTOToFreak) {
+        this.freakDTOToFreak = freakDTOToFreak;
+    }
+
+    @Autowired
+    public void setFreakToFreakDTO(FreakToFreakDTO freakToFreakDTO) {
+        this.freakToFreakDTO = freakToFreakDTO;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = {"/", ""})
+    public ResponseEntity<List> homePage() {
+        List<FreakDTO> freakDTOList =
+    }
 }
---
+++ src/main/java/org/academiadecodigo/converters/FreakDTOToFreak.java
@@ -0,0 +1,6 @@
+package org.academiadecodigo.converters;
+
+public class FreakDTOToFreak {
+
+    
+}
