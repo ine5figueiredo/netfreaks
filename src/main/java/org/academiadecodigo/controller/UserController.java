4192004dfe7a50e94717245bcc5af199682146d6
Anna Katasonova <anna.katasonova7@gmail.com>
2021-04-09 11:32:53 +0100

    cross origin

--- src/main/java/org/academiadecodigo/controller/UserController.java
+++ src/main/java/org/academiadecodigo/controller/UserController.java
@@ -11,6 +11,7 @@ import org.academiadecodigo.service.UserService;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/home")
 public class UserController {
