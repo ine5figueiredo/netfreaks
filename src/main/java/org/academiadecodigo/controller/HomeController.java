e8b06ab8e240686c85076f8e723c2b0016940d57
Inês Figueiredo <inesgomesfigueiredo@gmail.com>
2021-04-09 04:29:06 +0100

    Trying to get customer credentials.

--- src/main/java/org/academiadecodigo/controller/HomeController.java
+++ src/main/java/org/academiadecodigo/controller/HomeController.java
@@ -2,7 +2,9 @@ package org.academiadecodigo.controller;
 
 import org.academiadecodigo.command.SignInDto;
 import org.academiadecodigo.command.UserDto;
+import org.academiadecodigo.model.User;
 import org.academiadecodigo.service.LoginService;
+import org.academiadecodigo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,10 +14,16 @@ import org.springframework.web.bind.annotation.*;
 public class HomeController {
 
     private LoginService loginService;
+    private UserService userService;
 
     @Autowired
     public void setLoginService(LoginService loginService) {
         this.loginService = loginService;
+    }
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
     }
 
     @RequestMapping(method = RequestMethod.GET, path = {"", "/", "/home"})
@@ -26,7 +34,13 @@ public class HomeController {
     @RequestMapping(method = RequestMethod.GET, path = {"/home/login"})
     public ResponseEntity doLogIn(@RequestBody SignInDto signInDto) {
 
-        boolean authenticate = loginService.authenticate(signInDto.getUsername(), signInDto.getPassword());
+        User user = userService.getUser(signInDto.getUsername());
+
+        if (user == null) {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+
+        boolean authenticate = loginService.authenticate(user.getUsername(), user.getPassword());
 
         if (authenticate != true) {
             return new ResponseEntity(HttpStatus.BAD_REQUEST);
